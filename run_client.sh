a3f284dbc02106c3653b1e765e49f20dd09feffb
Enak <316165530@qq.com>
2017-08-11 18:14:35 +0800

    修改配置文件

---
+++ run_client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+echo $BASEDIR
+
+lua ./client/client.lua
+
---
+++ run_server.sh
@@ -0,0 +1,2 @@
+./3rd/redis/src/redis-server ./config/redis.conf
+./3rd/skynet/skynet ./config/skynet.conf
